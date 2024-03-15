3ca955e4f3bf4b8641beef1f891e7690eddb7057
charuzain <charuabhizain@gmail.com>
2024-03-15 04:41:07 -0400

    Style Result componenet for tablet and desktop

--- result-card/src/components/Result/Result.jsx
+++ result-card/src/components/Result/Result.jsx
@@ -5,12 +5,12 @@ const Result = () => {
       <div className="result__wrapper">
         <h2 className="result__title">Your result</h2>
         <div className="result__container">
-          <h1 className='result__score'>76</h1>
-          <p className='result__outof'>of 100</p>
+          <h1 className="result__score">76</h1>
+          <p className="result__outof">of 100</p>
         </div>
-        <p className='result__remarks'>Great</p>
-        <p className='result__summary'>
-          You scored higher than 65% of the people who have taken these tests.
+        <p className="result__remarks">Great</p>
+        <p className="result__summary">
+          Your performance exceed 65% of the people conducting the test here!
         </p>
       </div>
     </div>
