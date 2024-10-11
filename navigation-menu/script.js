5c5af7b68961571715c509bf348340529ee6fa64
charuzain <charuabhizain@gmail.com>
2024-10-11 00:26:59 -0400

    FInish mobile menu collapse and expand functionality

--- navigation-menu/script.js
+++ navigation-menu/script.js
@@ -1,13 +1,20 @@
-console.log('hi');
-
 const menuIcon = document.querySelector('#icon-menu');
-
+const navList = document.querySelector('.nav__list');
 
 menuIcon.addEventListener('click', () => {
-  const navList = document.querySelector('.nav__list');
   if (navList.style.display === '' || navList.style.display === 'none') {
     navList.style.display = 'block';
   } else {
     navList.style.display = 'none';
   }
+  overlay.classList.add('active');
+});
+
+const closeMenuIcon = document.querySelector('.mobile-menu');
+const overlay = document.getElementById('overlay');
+console.log(closeMenuIcon);
+
+closeMenuIcon.addEventListener('click', () => {
+  overlay.classList.remove('active');
+  navList.style.display = 'none';
 });
