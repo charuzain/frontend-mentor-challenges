4a01e231164375581deb0b085446210d2d626cbc
charuzain <charuabhizain@gmail.com>
2024-10-11 12:54:48 -0400

    Add style and expandable functioanlity

--- navigation-menu/script.js
+++ navigation-menu/script.js
@@ -27,16 +27,34 @@ for (let arrow of downArrows) {
     const nestedList = arrow.closest('.nav__nested');
     const nested = nestedList.querySelector('#nested');
 
-    // arrow.src = './images/icon-arrow-down.svg';
-    
     if (nested.style.display === 'none' || nested.style.display === '') {
+      arrow.src = './images/icon-arrow-up.svg';
       nested.style.display = 'block';
-          arrow.src = './images/icon-arrow-down.svg';
+    } else {
+      arrow.src = './images/icon-arrow-down.svg';
 
+      nested.style.display = 'none';
+    }
+  });
+}
+
+//
+
+const desktopDownArrows = document.querySelectorAll('.desktop-down-arrow');
+console.log(downArrows);
+
+for (let arrow of desktopDownArrows) {
+  arrow.addEventListener('click', () => {
+    const nestedList = arrow.closest('.nav-desktop__list-item');
+    const nested = nestedList.querySelector('#nested-desktop');
+
+    if (nested.style.display === 'none' || nested.style.display === '') {
+       arrow.src = './images/icon-arrow-up.svg';
+      nested.style.display = 'block';
+     
     } else {
       nested.style.display = 'none';
-          arrow.src = './images/icon-arrow-up.svg';
-
+      arrow.src = './images/icon-arrow-down.svg';
     }
   });
 }
