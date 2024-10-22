dd72418d47ea5f28963e5a60cfcda9973aee52ee
charuzain <charuabhizain@gmail.com>
2024-10-22 16:25:34 -0400

    Declare color variables

--- blog-preview/tailwind.config.js
+++ blog-preview/tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ['./src/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary:'hsl(var(--color-primary))',
+        light:'hsl(var(--color-light))',
+        gray950:'hsl(var(--color-gray950))',
+        gray500:'hsl(var(--color-gray500))',
+      },
+    },
   },
   plugins: [],
-}
-
+};
