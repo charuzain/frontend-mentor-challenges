9646d25c48596c9ffdefb465cf8f0c8c4c5c7a83
charuzain <charuabhizain@gmail.com>
2024-10-23 15:45:15 -0400

    Add custom font

--- blog-preview/tailwind.config.js
+++ blog-preview/tailwind.config.js
@@ -4,10 +4,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary:'hsl(var(--color-primary))',
-        light:'hsl(var(--color-light))',
-        gray950:'hsl(var(--color-gray950))',
-        gray500:'hsl(var(--color-gray500))',
+        primary: 'hsl(var(--color-primary))',
+        light: 'hsl(var(--color-light))',
+        gray950: 'hsl(var(--color-gray950))',
+        gray500: 'hsl(var(--color-gray500))',
+      },
+      boxShadow: {
+        blogShadow: '5px 5px rgba(0,0,0,1)',
+      },
+      fontFamily: {
+        figtree: ['Figtree', 'sans-serif'],
       },
     },
   },
